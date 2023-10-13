95782ea41058e45f303f2052cd77237d7738e2d5
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-10-14 01:31:04 +0700

    feat: Upload video component created

--- toktik-frontend/src/App.vue
+++ toktik-frontend/src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <UploadVideo />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import UploadVideo from "./components/UploadVideo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+    UploadVideo,
+  },
+};
 </script>
 
 <style>
