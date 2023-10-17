459b85c80c12e008799db2ce2b8838cdfe9a37b5
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-10-17 10:09:54 +0700

    feat: fe can fetch presigned url from be and upload to S3

--- toktik-frontend/src/main.js
+++ toktik-frontend/src/main.js
@@ -1,11 +1,14 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import App from './App.vue'
+// import '@babel/polyfill'
+// import 'mutationobserver-shim'
+import Vue from "vue";
+import "./plugins/bootstrap-vue";
+import App from "./App.vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
