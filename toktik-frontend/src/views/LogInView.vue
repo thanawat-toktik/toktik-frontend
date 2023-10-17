80214eeb176621f33210ed96bb0041108915aeee
thanawinboon <pokepat27@gmail.com>
2023-10-18 00:37:23 +0700

    made login & register api work

--- toktik-frontend/src/views/LogInView.vue
+++ toktik-frontend/src/views/LogInView.vue
@@ -58,26 +58,19 @@ export default {
   methods: {
     async onSubmit() {
       try {
-        console.log(this.email)
-        console.log(this.password)
-        const response = await fetch('http://localhost:8001/auth/jwt/create', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            email: this.email,
-            password: this.password
-          })
+        const formData = new FormData();
+        formData.append("email", this.email);
+        formData.append("password", this.password);
+
+        const response = await this.axios({
+          method: "POST",
+          url: `${process.env.VUE_APP_BACKEND_HOST}/auth/jwt/create/`,
+          data: formData,
+          // withCredentials: true
         });
 
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
+        console.log(`Response: ${response}`);
 
-        const data = await response.json();
-        console.log( data )
-        // Do something with the response data
       } catch (error) {
         this.error = 'An error occurred during login. Please try again.';
         console.error('Error:', error);
