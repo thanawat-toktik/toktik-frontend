9def589c9e955ba44eadabf2d09c4d9f27e1e0da
thanawinboon <pokepat27@gmail.com>
2023-10-19 03:43:21 +0700

    added code for dealing with 401

--- toktik-frontend/src/main.js
+++ toktik-frontend/src/main.js
@@ -16,7 +16,83 @@ new Vue({
   render: (h) => h(App),
 }).$mount("#app");
 
-let token = localStorage.getItem('jwt-token')
+const token = localStorage.getItem('jwt-token')
 if (token) {
   Vue.axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
 }
+const refresh_token = localStorage.getItem('jwt-token-refresh')
+if (refresh_token) {
+  Vue.axios.defaults.headers.common['Authorization'] = 'Bearer ' + refresh_token;
+}
+
+Vue.axios.interceptors.response.use(
+  (response) => {
+    return response
+  }, 
+  async (error) => {
+    console.log(error)
+    if (error.response.status === 401) {
+      const token = localStorage.getItem('jwt-token')
+      if (token) {
+        localStorage.removeItem('jwt-token')
+      }
+      const refresh_token = localStorage.getItem('jwt-token-refresh')
+      if (refresh_token) {
+        localStorage.removeItem('jwt-token-refresh')
+      }
+      router.push('/login')
+    }
+    return Promise.reject(error)
+  })
+
+// Vue.axios.interceptors.response.use(
+//   (response) => {
+//     return response
+//   }, 
+//   async (error) => {
+//     console.log(error)
+//     if (!error.response) {
+//       console.log("Unhandled error")
+//     }
+//     if (error.response.status === 401) {
+
+//       // checks if there's a pre-existing accesstoken
+//       const token = localStorage.getItem('jwt-token')
+//       if (!token) {
+//         router.push('/login')
+//       }
+//       if (token) { // remove existing token
+//         localStorage.removeItem('jwt-token')
+//       }
+      
+      
+//       // checks for refresh token
+//       const refresh_token = localStorage.getItem('jwt-token-refresh')
+//       if (!refresh_token) { // needs to log in if no refresh token
+//         router.push('/login')
+//       }
+
+//       try { // try to get new token
+//         const formData = new FormData();
+//         formData.append('refresh', refresh_token)
+
+//         const response = Vue.axios({
+//           method: "POST",
+//           url: `${process.env.VUE_APP_BACKEND_HOST}/auth/jwt/refresh/`,
+//           data: formData,
+//         });
+//         // TODO: solve why this returns nothing
+//         console.log(`Response: ${JSON.stringify(response)}`);
+//         // applies new token
+//         Vue.axios.defaults.headers.common['Authorization'] = 'Bearer ' + response.data.access;
+//         localStorage.setItem( 'jwt-token', response.data.access );
+//       }
+//       catch (error) {
+//         console.log(error)
+//         // forces user to get a new token
+//         localStorage.removeItem('jwt-token-refresh')
+//         await router.push({ name: "Log-In" });
+//       }      
+//     }
+//     return Promise.reject(error)
+//   })
