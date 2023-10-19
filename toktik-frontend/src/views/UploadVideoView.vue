bbe0c6ed12c0414b676bea3a9bb7474f0a824026
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-10-20 00:35:57 +0700

    feat: api calls don't use absolute url

--- toktik-frontend/src/views/UploadVideoView.vue
+++ toktik-frontend/src/views/UploadVideoView.vue
@@ -83,7 +83,7 @@ export default {
 
       const response = await this.axios({
         method: "POST",
-        url: `${process.env.VUE_APP_BACKEND_HOST}/video/upload-psurl/`,
+        url: `/api/video/upload-psurl/`,
         data: presignedUrlFormData,
       });
 
@@ -107,7 +107,7 @@ export default {
 
       const updateDBResponse = await this.axios({
         method: "POST",
-        url: `${process.env.VUE_APP_BACKEND_HOST}/video/update-db/`,
+        url: `/api/video/update-db/`,
         data: updateDBFormData,
       });
 
