cd445efc9e40186e78dc3ba79193bf53be2a7447
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-10-29 23:05:54 +0700

    feat: uses presigned playlist

--- toktik-frontend/src/components/VideoPlayer.vue
+++ toktik-frontend/src/components/VideoPlayer.vue
@@ -2,16 +2,16 @@
   <div v-show="videoLoaded">
     <h2>Video Player</h2>
     <video
-      ref="s3VideoPlayer"
-      id="video"
-      class="video-js vjs-default-skin"
-      preload="auto"
-      crossorigin="use-credentials"
-      controls
-      width="640"
-      height="268"
-      data-setup='{ "fluid": true }'
-      autoplay="true"
+        ref="s3VideoPlayer"
+        id="video"
+        class="video-js vjs-default-skin"
+        preload="auto"
+        crossorigin="use-credentials"
+        controls
+        width="640"
+        height="268"
+        data-setup='{ "fluid": true }'
+        autoplay="true"
     ></video>
   </div>
 </template>
@@ -28,36 +28,36 @@ export default {
     return {
       player: null,
       videoLoaded: false,
-      videoId: 2,
+      videoId: 25,
     };
   },
   mounted() {
     this.videoLoaded = false;
     this.getVideo();
   },
+  beforeDestroy() {
+    if (this.player.src()) {
+      URL.revokeObjectURL(this.player.src());
+    }
+    if (this.player) {
+      this.player.dispose();
+    }
+  },
   methods: {
     async getVideo() {
-      // this part sends an api call to backend for chunked vid
-      const payload = new FormData();
-      payload.append("video_ids", [this.videoId]);
-      payload.append("bucket", "chunked");
-
-      // const videoUrl = "https://chonky.toktik.thanawat.sgp1.cdn.digitaloceanspaces.com/IMG_6376_2/IMG_6376_2.m3u8"
       try {
         const response = await axios({
-          method: "POST",
+          method: "GET",
           url: `/api/video/get-url/`,
-          data: payload,
+          params: {
+            video_id: this.videoId,
+          },
           credentials: "include",
         });
-        let videoUrl;
-        if (!response.data) {
-          videoUrl = response.data.urls[0];
-        } else { // todo: remove this after 
-          videoUrl = "https://chonky.toktik.thanawat.sgp1.cdn.digitaloceanspaces.com/IMG_6376_2/IMG_6376_2.m3u8"
-        }
+        const playlistBlob = new Blob([response.data], {type: "text/plain"});
+        const playlistURL = URL.createObjectURL(playlistBlob);
         if (!this.player) {
-          this.initVideoPlayer(videoUrl);
+          this.initVideoPlayer(playlistURL);
         }
         this.videoLoaded = true;
         // TODO: incr view to the video with this id
