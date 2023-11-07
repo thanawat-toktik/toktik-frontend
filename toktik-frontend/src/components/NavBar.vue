7abfade41e26cd7e43579b6efd5f701ad0f83257
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-11-08 01:12:31 +0700

    feat: added bell icon

--- toktik-frontend/src/components/NavBar.vue
+++ toktik-frontend/src/components/NavBar.vue
@@ -1,48 +1,58 @@
 <template>
-  <b-navbar toggleable="sm" class="navbar" fixed="top">
+  <b-navbar toggleable="sm" class="navbar" fixed="top" justified>
     <b-navbar-brand href="#">
       <img
-        src="../assets/logo-icon-small.png"
-        alt="Toktik logo"
-        style="height: 35px"
+          src="../assets/logo-icon-small.png"
+          alt="Toktik logo"
+          style="height: 35px"
       />
       <img
-        src="../assets/logo-text.png"
-        alt="Toktik text"
-        style="height: 35px"
+          src="../assets/logo-text.png"
+          alt="Toktik text"
+          style="height: 35px"
       />
     </b-navbar-brand>
 
     <b-collapse id="nav-text-collapse" is-nav>
-      <b-navbar-nav align="right" fill>
+      <b-navbar-nav>
         <b-nav-item to="/feed" v-if="isLoggedIn">
-          <b-nav-text>Feed</b-nav-text>
+          <b-nav-text class="navbar-text">Feed</b-nav-text>
         </b-nav-item>
         <b-nav-item to="/" v-if="isLoggedIn">
-          <b-nav-text>My Videos</b-nav-text>
-        </b-nav-item>
-        <b-nav-item to="/login" v-else>
-          <b-nav-text>Login</b-nav-text>
-        </b-nav-item>
-        <b-nav-item to="/register" v-if="!isLoggedIn">
-          <b-nav-text>Register</b-nav-text>
+          <b-nav-text class="navbar-text">My Videos</b-nav-text>
         </b-nav-item>
         <b-nav-item to="/upload" v-if="isLoggedIn">
-          <b-nav-text>Upload</b-nav-text>
+          <b-nav-text class="navbar-text">Upload</b-nav-text>
+        </b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item to="/login" v-if="!isLoggedIn">
+          <b-nav-text class="navbar-text">Login</b-nav-text>
+        </b-nav-item>
+        <b-nav-item to="/register" v-if="!isLoggedIn">
+          <b-nav-text class="navbar-text">Register</b-nav-text>
         </b-nav-item>
         <b-nav-item v-if="isLoggedIn">
-          <b-nav-text v-on:click="onLogout">Logout</b-nav-text>
+          <b-iconstack scale="1.2">
+            <b-icon stacked style="color: black;" icon="bell"></b-icon>
+            <b-icon stacked style="color: white;" shift-h="5" shift-v="5" scale="0.55" icon="circle-fill"></b-icon>
+            <b-icon stacked style="color: red;" shift-h="5" shift-v="5" scale="0.45" icon="circle-fill"></b-icon>
+          </b-iconstack>
+        </b-nav-item>
+        <b-nav-item v-if="isLoggedIn">
+          <b-icon style="color: black; scale: 1.25;" icon="door-open" v-on:click="onLogout"></b-icon>
         </b-nav-item>
       </b-navbar-nav>
     </b-collapse>
-    <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+    <b-navbar-toggle style="background: white;" target="nav-text-collapse"></b-navbar-toggle>
   </b-navbar>
 </template>
 
 <script>
 import router from "@/router";
 import axios from "@/axios";
-import { EventBus } from "@/eventBus";
+import {EventBus} from "@/eventBus";
 
 export default {
   name: "NavBar",
@@ -79,17 +89,23 @@ export default {
         title: "Log-out Successful",
         message: "You are now logged out!",
       });
-      await router.push({ name: "login" });
+      await router.push({name: "login"});
     },
   },
 };
 </script>
 
-<style>
+<style scoped>
 .navbar {
-  background-color: #fff;
+  background-color: white;
   height: 60px;
   border-bottom: 1px solid lightgrey;
   margin-bottom: -1px;
+}
+
+.navbar-text {
+  font-weight: 700;
+  font-size: 18px;
+  color: #111111;
 }
 </style>
