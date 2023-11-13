558df42526b812c498e5c90138c181737adb28a6
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-11-13 23:11:16 +0700

    fix: REANABLED THE WEBSOCKET

--- toktik-frontend/vue.config.js
+++ toktik-frontend/vue.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    webSocketServer: false,
     proxy: {
       "/api": {
         target: process.env.VUE_APP_BACKEND_HOST,
